f185ca22ad064c29051ae5af260937ab15e85bc6
IlyaKrass <36201691+Ilia-2004@users.noreply.github.com>
2023-11-11 21:09:50 +0400

    Initial commit

---
+++ check_len_solution/check_len_str.cpp
@@ -0,0 +1,38 @@
+﻿#include <iostream>
+
+extern "C" int longStr(char* str)
+{
+    int length;
+    _asm {
+            mov ecx, 0
+            mov al, str[ecx]
+            mov bl, 0
+
+            cikl:
+            cmp al, 0
+                je endd
+
+                inc ecx
+                mov al, str[ecx]
+                inc bl
+
+                jmp cikl
+
+                endd :
+            mov length, ecx
+    }
+    return length;
+}
+
+int main()
+{
+    char str[100];
+
+    std::cout << "Введите строку: ";
+    std::cin >> str;
+
+    int length = longStr(str);
+
+    std::cout << "Длина строки: " << length;
+    return 0;
+}
